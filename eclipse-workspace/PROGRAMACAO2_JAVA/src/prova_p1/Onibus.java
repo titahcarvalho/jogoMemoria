5761565423e61ea3f6c2782e38b46738dbc8a304
Tita <titahcarvalho@hotmail.com>
2023-12-04 09:54:56 -0300

    first commit

---
+++ eclipse-workspace/PROGRAMACAO2_JAVA/src/prova_p1/Onibus.java
@@ -0,0 +1,21 @@
+package prova_p1;
+
+public class Onibus {
+	private int numero;
+	private double km;
+	
+	public Onibus(int numero, double km) {
+		this.numero = numero;
+		this.km = km;
+	}
+
+	public double getKm() {
+		return km;
+	}
+
+	public void setKm(double km) {
+		this.km = km;
+	}
+
+	
+}
